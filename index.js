8348bc1896684a3c6924511bd4f50791838e00fb
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-08-06 20:21:43 +0530

    PS3

--- index.js
+++ index.js
@@ -25,3 +25,12 @@ for(var i=1; i<=10; i+=1){
     nums += i + " ";
 }
 console.log(nums);
+
+//3.Print the odd numbers less than 100
+var odds = "";
+for(var i=1; i<=100; i+=1){
+    if(i%2 != 0){
+        odds += i + " ";
+    }
+}
+console.log(odds);
