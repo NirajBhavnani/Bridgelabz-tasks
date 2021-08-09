cd189b07a9adc44244a41b056289031aa771518f
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-08-09 21:56:23 +0530

    Find all duplicate numbers in an array

--- index.js
+++ index.js
@@ -200,3 +200,34 @@ for(var i=0; i<pairArr.length; i++){
     }
 }
 console.log(pairs);
+
+//17.How do you find duplicate numbers in an array if it contains multiple duplicates?
+duplicate = [];
+dupliIndex = [];
+dupliArr = [10, 65, 58, 10, 23, 42, 10];
+boolDup = false;
+if(checkDuplicate(dupliArr)){
+    for(var i=0; i<dupliArr.length; i++){
+        if(dupliArr[i]===duplicate[0]){
+            dupliIndex.push(i);
+        }
+    }        
+}
+console.log(dupliIndex);
+
+function checkDuplicate(arr){
+    for(var i=0; i<arr.length; i++){
+        for(var j=0; j<arr.length;j++){
+            if(i!==j){//Don't check same elements
+                if(arr[i] == arr[j]){
+                    duplicate.push(arr[i]);
+                    boolDup = true;
+                    break;
+                }
+            }
+            if(boolDup){
+                return true;
+            }
+        }
+    }
+}
